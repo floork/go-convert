f628ae2f8cb86c3782bfe5bd67cff7e014ef14d4
Florian Mros <florian_alexander.mros@tu-dresden.de>
2024-03-20 18:43:47 +0100

    add: working converting

--- go.mod
+++ go.mod
@@ -3,7 +3,19 @@ module go-convert
 go 1.22.1
 
 require (
+	github.com/disintegration/imaging v1.6.2 // indirect
+	github.com/hhrutter/lzw v1.0.0 // indirect
+	github.com/hhrutter/tiff v1.0.1 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/pdfcpu/pdfcpu v0.7.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
 	github.com/spf13/cobra v1.8.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/sunshineplan/imgconv v1.1.9 // indirect
+	github.com/sunshineplan/pdf v1.0.7 // indirect
+	golang.org/x/image v0.15.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,30 +1,97 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
 
-	"github.com/spf13/cobra"
+	"github.com/sunshineplan/imgconv"
 )
 
 func main() {
-	rootCmd := &cobra.Command{
-		Use:   "myapp",
-		Short: "A brief description of your application",
-		Long:  `A longer description that spans multiple lines and likely contains examples and usage.`,
-		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Println("Hello, Cobra!")
-		},
+	// Define flags
+	targetFormat := flag.String("target", "png", "target format")
+	noDel := flag.Bool("no-delete", false, "do not delete original images")
+
+	// Parse flags
+	flag.Parse()
+
+	// Check if help flag is provided
+	if flag.NArg() == 0 || flag.Arg(0) == "-h" || flag.Arg(0) == "--help" {
+		printUsage()
+		return
 	}
 
-	rootCmd.AddCommand(&cobra.Command{
-		Use:   "subcommand",
-		Short: "A brief description of the subcommand",
-		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Println("Running subcommand")
-		},
-	})
+	// Get source file path
+	sourceFile := flag.Arg(0)
 
-	if err := rootCmd.Execute(); err != nil {
-		fmt.Println(err)
+	if flag.NArg() != 1 {
+		printUsage()
+		return
 	}
+
+	// Extract file extension using filepath.Ext
+	fileExt := filepath.Ext(sourceFile)
+
+	// Validate target format
+	formattedTargetFormat := strings.ToLower(*targetFormat)
+	allowedFormats := []string{"jpg", "png", "gif", "webp", "pdf"}
+	if !contains(allowedFormats, *targetFormat) {
+		fmt.Printf("Unsupported target format: %s\n", *targetFormat)
+		return
+	}
+
+	// Open source image
+	src, err := imgconv.Open(sourceFile)
+	if err != nil {
+		fmt.Println("Error opening source image:", err)
+		return
+	}
+
+	// Generate target file path with the correct extension
+	sourceName := strings.TrimSuffix(sourceFile, fileExt)
+	target := fmt.Sprintf("%s.%s", sourceName, formattedTargetFormat)
+
+	// Convert and save image
+	outputFile, err := os.Create(target)
+	if err != nil {
+		fmt.Println("Error creating output file:", err)
+		return
+	}
+	defer outputFile.Close()
+
+	// Define map for converting target format string to imgconv.Format
+	lookUp := map[string]imgconv.Format{
+		"jpg": imgconv.JPEG,
+		"png": imgconv.PNG,
+		"gif": imgconv.GIF,
+		"pdf": imgconv.PDF,
+	}
+
+	// Convert and write image
+	if err := imgconv.Write(outputFile, src, &imgconv.FormatOption{Format: lookUp[formattedTargetFormat]}); err != nil {
+		fmt.Println("Error converting and writing image:", err)
+		return
+	}
+
+	if !*noDel {
+		fmt.Println("Original image not deleted.")
+	}
+}
+
+func printUsage() {
+	fmt.Println("Usage: go-convert [options] <source>")
+	fmt.Println("Options:")
+	flag.PrintDefaults()
+}
+
+func contains(slice []string, str string) bool {
+	for _, s := range slice {
+		if s == str {
+			return true
+		}
+	}
+	return false
 }
